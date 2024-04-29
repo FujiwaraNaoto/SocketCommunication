6c544171cbc38d46f44e9e36d86a0b7871ed4271
FujiwaraNaoto <naotofujiwara890@gmail.com>
2024-04-29 19:42:46 +0900

    add function to tcp_server.cpp

--- socket_cpp/tcp_server.cpp
+++ socket_cpp/tcp_server.cpp
@@ -16,7 +16,7 @@ int main(){
 
     try{
 
-        boost::asio::io_context io_context;
+        asio::io_context io_context;
         tcp::endpoint endpoint=tcp::endpoint(tcp::v4(),port);
         tcp::acceptor acceptor(io_context,endpoint);
         std::cout<<"Endpoint IP address: "<<endpoint.address().to_string()<<std::endl;
@@ -28,12 +28,35 @@ int main(){
         {
             tcp::socket socket(io_context);
             acceptor.accept(socket);
+            std::cout<<"Client connected"<<std::endl;
 
+            {//メッセージを受け取った処理
+                std::array<char,1<<7> buf;
+                boost::system::error_code error;
+                
+                size_t message_len=socket.read_some(asio::buffer(buf),error);
+                //std::cout<<"message_len="<<message_len<<std::endl;
+                for(size_t idx=0;idx<message_len;idx++){
+                   std::cout<<buf[idx];
+                }
+                std::cout<<std::endl;
+
+                if(error==asio::error::eof){
+                    std::cout<<"Connection closed"<<std::endl;
+                    
+                }else if(error){
+                    throw boost::system::system_error(error);
+                }
+                
+                asio::write(socket,asio::buffer(buf,message_len));
+
+            }
+            /*
             std::string message="Hello from server\n";
 
             boost::system::error_code ignored_error;
             boost::asio::write(socket,boost::asio::buffer(message),ignored_error);
-
+            */
         }
         
         
